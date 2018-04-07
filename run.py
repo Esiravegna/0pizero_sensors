371ca20526cf41d5fa3f59394720b5e8b9732710
Esteban <esiravegna@gmail.com>
2018-04-07 14:56:56 -0300

    Adding daemon

--- run.py
+++ run.py
@@ -4,14 +4,16 @@ from utils.log import log
 
 import click
 from daemonocle.cli import DaemonCLI
-
 log = log.name(__name__)
-@click.command(cls=DaemonCLI, daemon_params={'pidfile': '/var/run/opi0sensors.pid'})
-def run_daemon():
-	log.info("Starting daemon...")
-	s = SensorsController()
-	s.start()
 
+
+@click.command(cls=DaemonCLI, daemon_params={
+	'pidfile': '/var/run/opi0sensors.pid'
+	})
+def run_daemon():
+	s = SensorsController()
+	log.info("Starting daemon...")
+	s.start()
 
 if __name__ == '__main__':
     run_daemon() 
